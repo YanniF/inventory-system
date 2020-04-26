66efa8b01a0b4d1c94fa81a9acceea13b9514ade
YanniF <yanni_fernanda@hotmail.com>
2020-04-26 21:55:28 +0200

    Backend: CRUD items

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -1,6 +1,6 @@
 const functions = require('firebase-functions');
 const app = require('express')();
-const { db } = require('./util/admin');
+const fbAuth = require('./util/fbAuth');
 
 const { register, login } = require('./handlers/users');
 const { addItem, getAllItems, getItem, editItem, deleteItem } = require('./handlers/items');
@@ -8,10 +8,10 @@ const { addItem, getAllItems, getItem, editItem, deleteItem } = require('./handl
 app.post('/register', register);
 app.post('/login', login);
 
-app.post('/items', addItem);
-app.get('/items', getAllItems);
-app.get('/items/:itemId', getItem);
-app.put('/items/:itemId', editItem);
-app.delete('/items/:itemId', deleteItem);
+app.post('/items', fbAuth, addItem);
+app.get('/items', fbAuth, getAllItems);
+app.get('/items/:itemId', fbAuth, getItem);
+app.put('/items/:itemId', fbAuth, editItem);
+app.delete('/items/:itemId', fbAuth, deleteItem);
 
 exports.api = functions.region('europe-west1').https.onRequest(app);
